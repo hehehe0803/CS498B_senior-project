2fab95d922b810ba4a8f3919712ba4787da9e953
Hoang V. Pham <hehehe0803@gmail.com>
2021-03-29 23:04:02 +0700

    add keras image detection model

--- main.py
+++ main.py
@@ -1,15 +1,47 @@
-import numpy as np
 import cv2
+from PIL import Image, ImageOps
+from keras import models
+import numpy as np
+
 
 def main():
+    model = models.load_model('./detect_model/keras_model.h5')
     cap = cv2.VideoCapture(0)
 
-    while(True):
+    categories = ['Skull', 'Pikachu', 'Man', 'House']
+
+    while (True):
         # Capture frame-by-frame
         ret, frame = cap.read()
 
+        # Create the array of the right shape to feed into the keras model
+        # The 'length' or number of images you can put into the array is
+        # determined by the first position in the shape tuple, in this case 1.
+        data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
+        # Convert the captured frame into RGB
+        image = Image.fromarray(frame, 'RGB')
+
+        # resize the image to a 224x224 with the same strategy as in TM2:
+        # resizing the image to be at least 224x224 and then cropping from the center
+        size = (224, 224)
+        image = ImageOps.fit(image, size, Image.ANTIALIAS)
+
+        # turn the image into a numpy array
+        image_array = np.asarray(image)
+
+        # Normalize the image
+        normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+
+        # Load the image into the array
+        data[0] = normalized_image_array
+
+        # run the inference
+        prediction = model.predict(data)
+        print(categories[np.argmax(prediction)])
+
         # Display the resulting frame
-        cv2.imshow('frame',frame)
+        cv2.imshow('frame', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
@@ -17,5 +49,6 @@ def main():
     cap.release()
     cv2.destroyAllWindows()
 
+
 if __name__ == "__main__":
-        main()
+    main()
